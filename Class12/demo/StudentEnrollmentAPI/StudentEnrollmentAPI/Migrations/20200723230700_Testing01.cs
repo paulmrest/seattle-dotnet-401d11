642087019af568c00b4c2c167d9e0e5a9e8c5860
paulmrest <paulmrest@gmail.com>
2020-07-29 14:38:26 -0700

    Updating

---
+++ Class12/demo/StudentEnrollmentAPI/StudentEnrollmentAPI/Migrations/20200723230700_Testing01.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentEnrollmentAPI.Migrations
+{
+    public partial class Testing01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transcript",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(nullable: false),
+                    CourseId = table.Column<int>(nullable: false),
+                    Passed = table.Column<bool>(nullable: false),
+                    Grade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transcript", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Transcript_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transcript_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transcript_CourseId",
+                table: "Transcript",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transcript_StudentId",
+                table: "Transcript",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transcript");
+        }
+    }
+}
